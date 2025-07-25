95e5db707950e211875499d658054fbad61165dd
Dreadbug <201382028+Dreadbug@users.noreply.github.com>
2025-07-25 12:36:20 -0400

    Initial commit
    The first version of the game. Does not include any gameplay, just the start of the character creator.

---
+++ script.js
@@ -0,0 +1,189 @@
+// This file contains all the behavior for some of the html elements
+// It also holds the game behavior
+
+
+// Establish player
+let playerParty
+if (sessionStorage.getItem("playerParty")) {
+    playerParty = JSON.parse(sessionStorage.getItem("playerParty"))
+}
+else {
+    playerParty = {player:'You',wife:'Petra',daughter:'Margeratta',son:'Gunther'}
+}
+
+let playerHealth
+if (sessionStorage.getItem("playerHealth")) {
+    playerHealth = JSON.parse(sessionStorage.getItem("playerHealth"))
+}
+else {
+    playerHealth = {player:0,wife:0,daughter:0,son:0}
+}
+
+let playerItems
+if (sessionStorage.getItem("playerItems")) {
+    playerItems = JSON.parse(sessionStorage.getItem("playerItems"))
+}
+else {
+    playerItems = {food:0,arrows:0,money:100,herbs:0,wheel:0,axle:0,tongue:0,clothes:0,oxen:0}
+}
+
+let scoreMultiplier
+if (sessionStorage.getItem("scoreMultiplier")) {
+    scoreMultiplier = JSON.parse(sessionStorage.getItem("scoreMultiplier"))
+}
+else {
+    scoreMultiplier = 1.0
+}
+
+
+// Establish the season/weather and related difficulty
+let season
+if (sessionStorage.getItem("season")) {
+    season = JSON.parse(sessionStorage.getItem("season"))
+}
+else {
+    season = "fall"
+}
+
+let seasonTemp
+if (sessionStorage.getItem("seasonTemp")) {
+    seasonTemp = JSON.parse(sessionStorage.getItem("seasonTemp"))
+}
+else {
+    seasonTemp = 10
+}
+
+// Keep track of days and distance
+let distance = 0
+let days = 0
+
+let date
+if (sessionStorage.getItem("date")) {
+    date = JSON.parse(sessionStorage.getItem("date"))
+}
+else {
+    date = "1/10/1347"
+}
+
+
+// Button on story screen
+const storyText = document.getElementById("storytext")
+let numClicks = 0
+function changeTitleText() {
+    numClicks += 1
+    if (numClicks == 1) {
+        storyText.innerHTML = "...Medieval Europe. 1347. A time of sorrow and suffering..."
+    }
+    else if (numClicks == 2) {
+        storyText.innerHTML = "...the Plague ravages the wicked and the righteous alike..."
+    }
+    else if (numClicks == 3) {
+        storyText.innerHTML = "...no one is safe from its evil influence..."
+    }
+    else if (numClicks == 4) {
+        storyText.innerHTML = "...someone needs to bring the light to this forsaken place..."
+    }
+    else if (numClicks == 5) {
+        storyText.innerHTML = "...Doctor, we need you more than ever."
+    }
+    else {
+        window.location.href = "creator.html"
+    }
+}
+
+// Add names (if any were written) to playerParty
+function changePartyNames() {
+    const newWifeName = document.getElementById("wifename").value
+    const newDaughterName = document.getElementById("daughtername").value
+    const newSonName = document.getElementById("sonname").value
+    if (newWifeName != "") {
+        playerParty["wife"] = newWifeName
+    }
+    if (newDaughterName != "") {
+        playerParty["daughter"] = newDaughterName
+    }
+    if (newSonName != "") {
+        playerParty["son"] = newSonName
+    }
+    // Add localStorage json saving and apply to shop.html
+    sessionStorage.setItem("playerParty",JSON.stringify(playerParty))
+    window.location.href = "creator_2.html"
+}
+
+// Update money based on occupation
+function changeJobMoney(job) {
+    if (job == "farmer") {
+        playerItems["money"] = 50
+        playerItems["food"] = 200
+        scoreMultiplier = 2.0
+        document.getElementById("farmeroption").style.color = "green"
+        document.getElementById("barberoption").style.color = "white"
+        document.getElementById("doctoroption").style.color = "white"
+    }
+    else if (job == "medic") {
+        playerItems["money"] = 200
+        playerItems["herbs"] = 50
+        document.getElementById("farmeroption").style.color = "white"
+        document.getElementById("barberoption").style.color = "white"
+        document.getElementById("doctoroption").style.color = "green"
+        scoreMultiplier = 1.0
+    }
+    else {
+        playerItems["money"] = 125
+        document.getElementById("farmeroption").style.color = "white"
+        document.getElementById("barberoption").style.color = "green"
+        document.getElementById("doctoroption").style.color = "white"
+        scoreMultiplier = 0.25
+    }
+
+    // Save to session storage
+    sessionStorage.setItem("playerItems",JSON.stringify(playerItems))
+    sessionStorage.setItem("scoreMultiplier",JSON.stringify(scoreMultiplier))
+}
+
+// Update a bunch of difficulty variables based on chosen season
+function changeInitialSeason(chosenSeason) {
+    season = chosenSeason
+    if (chosenSeason == "winter") {
+        seasonTemp = 0
+        date = "1/1/1347"
+        document.getElementById("winteroption").style.color = "royalblue"
+        document.getElementById("springoption").style.color = "white"
+        document.getElementById("summeroption").style.color = "white"
+        document.getElementById("falloption").style.color = "white"
+    }
+    else if (chosenSeason == "spring") {
+        seasonTemp = 15
+        date = "1/4/1347"
+        document.getElementById("winteroption").style.color = "white"
+        document.getElementById("springoption").style.color = "hotpink"
+        document.getElementById("summeroption").style.color = "white"
+        document.getElementById("falloption").style.color = "white"
+    }
+    else if (chosenSeason == "summer") {
+        seasonTemp = 20
+        date = "1/7/1347"
+        document.getElementById("winteroption").style.color = "white"
+        document.getElementById("springoption").style.color = "white"
+        document.getElementById("summeroption").style.color = "orange"
+        document.getElementById("falloption").style.color = "white"
+    }
+    else {
+        seasonTemp = 10
+        date = "1/10/1347"
+        document.getElementById("winteroption").style.color = "white"
+        document.getElementById("springoption").style.color = "white"
+        document.getElementById("summeroption").style.color = "white"
+        document.getElementById("falloption").style.color = "sienna"
+    }
+
+    // Save to session storage
+    sessionStorage.setItem("season",JSON.stringify(season))
+    sessionStorage.setItem("seasonTemp",JSON.stringify(seasonTemp))
+    sessionStorage.setItem("date",JSON.stringify(date))
+}
+
+// For checking if values are changed
+function logAValue() {
+    console.log(playerParty)
+}
